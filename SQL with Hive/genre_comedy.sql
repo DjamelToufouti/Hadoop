a0a7363ee030671769a422c3a3e857bdffd87777
DjamelToufouti <66113486+DjamelToufouti@users.noreply.github.com>
2020-10-02 10:35:11 +0200

    Update genre_comedy.sql

--- SQL with Hive/genre_comedy.sql
+++ SQL with Hive/genre_comedy.sql
@@ -1,7 +1,5 @@
-SELECT imdb_title_basics.originaltitle, imdb_title_basics.genres, imdb_title_ratings.averagerating
-FROM imdb_title_basics
-JOIN imdb_title_ratings
-ON imdb_title_basics.tconst = imdb_title_ratings.tconst
-WHERE ARRAY_CONTAINS (imdb_title_basics.genres, "Comedy")
-ORDER BY imdb_title_ratings.averagerating DESC
+SELECT A.originaltitle, A.genres, B.averagerating FROM imdb_title_basics A
+  JOIN imdb_title_ratings B ON A.tconst = B.tconst
+WHERE ARRAY_CONTAINS (A.genres, "Comedy")
+ORDER BY B.averagerating DESC
 LIMIT 100;
